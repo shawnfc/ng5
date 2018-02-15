8d1a2fc44941a0133fe2327edf3bffc6e9ccda8c
Shawn Conrad <shawnconrad@shawns-mbp.attlocal.net>
2018-02-15 18:08:37 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+// the component decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'app';
 }
